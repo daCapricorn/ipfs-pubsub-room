5b09febfab3cf62ec30f41ce6c87235a5ae9bf6a
kevingzhang <kevin.zhang.canada@gmail.com>
2019-08-24 11:05:47 -0700

    use uuid

--- src/index.js
+++ src/index.js
@@ -10,6 +10,8 @@ const Connection = require('./connection')
 const encoding = require('./encoding')
 const directConnection = require('./direct-connection-handler')
 const libp2p = require('./libp2p')
+const uuidv1 = require('uuid/v1');
+
 
 const DEFAULT_OPTIONS = {
   pollInterval: 1000
@@ -281,18 +283,6 @@ class PubSubRoom extends EventEmitter {
 
 
   _generateUUID() { // Public Domain/MIT
-    var d = new Date().getTime();//Timestamp
-    var d2 = (performance && performance.now && (performance.now()*1000)) || 0;//Time in microseconds since page-load or 0 if unsupported
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-        var r = Math.random() * 16;//random number between 0 and 16
-        if(d > 0){//Use timestamp until depleted
-            r = (d + r)%16 | 0;
-            d = Math.floor(d/16);
-        } else {//Use microseconds since page-load if supported
-            r = (d2 + r)%16 | 0;
-            d2 = Math.floor(d2/16);
-        }
-        return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
-    });
+    return uuidv1();
   }
 }
